e4c2f8853a2167ee3186579c52b3b7f0e9f23753
Alessandro Nerla <nerla.alessandro@gmail.com>
2019-10-06 18:53:45 +0100

    Update ServerChat.java

--- Server Client Chat/ServerChat.java
+++ Server Client Chat/ServerChat.java
@@ -7,7 +7,7 @@ import java.util.Scanner;
 
 public class ServerChat {
 	
-	int portNumber;
+	int portNumber; //Use ports above 1024 to avoid conflicts (Or use Doom's port for the nostalgia)
 	
 	public ServerChat(int portNumber) {
 		this.portNumber = portNumber;
@@ -23,19 +23,19 @@ public class ServerChat {
 			String messageIn = "";
 			String messageOut = "";
 			System.out.println("Online");
-			while(!connectionSocket.isClosed()) {
-				if(socketIn.available() != 0) {
+			while(!connectionSocket.isClosed()) { //Loops while the connection is open
+				if(socketIn.available() != 0) { //Executes if there is an inbound message
 					messageIn = DataInputStream.readUTF(socketIn);
-					if(messageIn.contains("END CONNECTION")) {
+					if(messageIn.contains("END CONNECTION")) { //Recieving "END CONNECTION" closes the connection
 						connectionSocket.close();
 					} else {
 						System.out.println(messageIn);
 					}
 				}
-				if(System.in.available() != 0) {
+				if(System.in.available() != 0) { //Executes if there is an outbound message
 					messageOut = "[Server]: ";
 					messageOut += textInput.nextLine();
-					if(messageOut.contains("END CONNECTION")) {
+					if(messageOut.contains("END CONNECTION")) { //Sending "END CONNECTION" closes the connection
 						connectionSocket.close();
 					} else {
 						socketOut.writeUTF(messageOut);
